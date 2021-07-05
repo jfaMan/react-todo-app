ddda5cbc9d2a9e9c3b859031c7e8d1ffcb731c63
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-07-05 23:55:50 +0900

    adding jokes data

--- src/components/AnimalCard.js
+++ src/components/AnimalCard.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Joke from './Joke';
+import jokesData from './jokesData';
 
 const AnimalCard = (props) => {
+
+  const jokesComponents = jokesData.map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />);
+  console.log(jokesComponents);
+
   return (
     <div className='contact-card'>
       <h3>{props.contact.name}</h3>
@@ -10,6 +15,9 @@ const AnimalCard = (props) => {
       <Joke
         joke = {{question: "Guess who?", answer: `Ya boy, ${props.contact.name}!`}}
       />
+      <div>
+        {jokesComponents}
+      </div>
     </div>
   )
 };
