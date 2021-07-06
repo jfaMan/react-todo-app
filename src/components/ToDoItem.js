646295eb50cf839c7fcd8fb52ae9b18718591d11
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-07-06 18:41:08 +0900

    checked boxes

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ToDoItem = (props) => {
   return (
     <div className="todo-item">
-      <input type="checkbox" />
+      <input type="checkbox" checked={props.item.completed}/>
       <p>{props.item.text}</p>
     </div>
   )
