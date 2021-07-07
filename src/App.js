b294e0889bc1e06068a45b84b08bd664079c44e7
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-07-07 18:16:06 +0900

    changed function component to class component and added state

--- src/App.js
+++ src/App.js
@@ -4,32 +4,39 @@ import ToDoItem from './components/ToDoItem';
 import AnimalCard from './components/AnimalCard';
 import todosData from './components/todosData';
 
-const App = () => {
-
-  const todoItems = todosData.map(item => <ToDoItem key={item.id} item={item} />)
-
-  return (
-    <div>
-      <div className="todo-list">
-        <Header />
-        {todoItems}
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+  }
+  render() {
+    const todoItems = this.state.todos.map(item => <ToDoItem key={item.id} item={item} />)
+    return (
+      <div>
+        <div className="todo-list">
+          <Header />
+          {todoItems}
+        </div>
+        <div className='contacts'>
+          <AnimalCard 
+            contact = {{name: "Wolfy", type: "dog", age: "3"}}
+          />
+          <AnimalCard
+            contact = {{name: "Tom", type: "cat", age: "8"}}
+          />
+          <AnimalCard
+            contact = {{name: "Jerry", type: "mouse", age: "4"}}
+          />
+          <AnimalCard
+            contact = {{name: "Daffy", type: "duck", age: "16"}}
+          />
+        </div>
       </div>
-      <div className='contacts'>
-        <AnimalCard 
-          contact = {{name: "Wolfy", type: "dog", age: "3"}}
-        />
-        <AnimalCard
-          contact = {{name: "Tom", type: "cat", age: "8"}}
-        />
-        <AnimalCard
-          contact = {{name: "Jerry", type: "mouse", age: "4"}}
-        />
-        <AnimalCard
-          contact = {{name: "Daffy", type: "duck", age: "16"}}
-        />
-      </div>
-    </div>
-  )
+    )
+  }
+
 };
 
 export default App;
